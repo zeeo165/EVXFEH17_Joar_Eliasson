770e2f97ed0448ca3ef71bcc5808017c8453c0f7
zeeo165 <joar.eliasson149@gmail.com>
2018-01-15 17:48:03 +0100

    initial commit

---
+++ Agila/base_project-master/README.md
@@ -0,0 +1,13 @@
+# Gulp Starter 
+
+Install sassdoc globally:  
+`npm install sassdoc -g`  
+clone https://github.com/mig-25/base_project.git  
+Remember to run the `npm install` command after cloning this repo 
+You might need to change port nr on the localhost url in the gulpfile for sassdoc depending on which port localhost laucnhes on, or change to a exeternal url      
+Then run `gulp`  
+To see the sassdoc run `gulp sassdoc` a sassdoc directory will be created on the root of the project, run the index.html file within  
+For every change in your sass files, a new  `gulp sassdoc` command must be executed to reflect the changes in sassdocs  
+:) 
+
+
--- Agila/base_project-master/gulpfile.js
+++ Agila/base_project-master/gulpfile.js
@@ -6,15 +6,14 @@ var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
+var notify = require('gulp-notify');
 var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
+var sassdoc = require('sassdoc');
 var louis = require('gulp-louis');
 
-// Basic Gulp task syntax
-gulp.task('hello', function() {
-  console.log('Hello Student!');
-})
+
 
 // Development Tasks 
 // -----------------
@@ -23,15 +22,17 @@ gulp.task('hello', function() {
 gulp.task('browserSync', function() {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: 'app', 
+      index: 'index.html'
     }
   })
 })
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
@@ -55,16 +56,19 @@ gulp.task('useref', function() {
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
+    
 });
 
 // Optimizing Images 
 gulp.task('images', function() {
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
     // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
+   .pipe(cache(imagemin({
+      optimizationLevel: 5, progressive: true, interlaced: true
     })))
     .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+
 });
 
 // Copying fonts 
@@ -72,6 +76,15 @@ gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'))
 })
+
+//Sass documentation
+gulp.task('sassdoc', function () {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sassdoc())
+    .resume();
+});
+
 
 // Cleaning 
 gulp.task('clean', function() {
@@ -84,23 +97,28 @@ gulp.task('clean:dist', function() {
   return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
 });
 
-
+//Performance budget
 gulp.task('louis', function() {
   louis({
-    url: 'http://localhost:3000/',
-    timeout: 200,
+    timeout: 60,
+    url: 'http://localhost:3000',
     performanceBudget: {
-      requests: 10,
-      domComplete: 3000
+      cssSize: 30000,
+      imageSize: 900000,
+      
     }
-  })
+  });
 });
+
+
+ 
+
 
 // Build Sequences
 // ---------------
 
 gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
+  runSequence(['sass', 'browserSync', 'watch', 'louis'],
     callback
   )
 })
@@ -109,7 +127,7 @@ gulp.task('build', function(callback) {
   runSequence(
     'clean:dist',
     'sass',
-    ['useref', 'images', 'fonts'],
+    ['useref', 'images', 'fonts', 'sassdoc'],
     callback
   )
 })
